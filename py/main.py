2139f43628ced0489b57f34108d44a015bbb4c07
Райот (Riot) <daxkod3243@gmail.com>
2023-01-01 00:11:06 +0300

    Add files via upload

---
+++ py/main.py
@@ -0,0 +1,73 @@
+from time import sleep
+from colorama import Fore, Back, Style
+
+sleep(5)
+print(Fore.RED)
+print('Dashing through the snow')
+
+sleep(3)
+print(Fore.GREEN)
+print('In a one horse open sleigh,')
+
+sleep(2)
+print(Fore.YELLOW)
+print("O'er fields we go,")
+
+sleep(2)
+print(Fore.CYAN)
+print('Laughing all the way;')
+
+sleep(3)
+print(Fore.RED)
+print('Bells on bob-tail ring,')
+
+sleep(2)
+print(Fore.GREEN)
+print('Making spirits bright,')
+
+sleep(3)
+print(Fore.YELLOW)
+print('What a fun it is to ride and sing a sleighing song tonight!')
+
+sleep(4)
+print(Fore.CYAN)
+print('Jingle bells, Jingle bells,')
+
+sleep(2)
+print(Fore.RED)
+print('Jingle all the way!')
+
+sleep(3)
+print(Fore.GREEN)
+print('O what fun it is to ride')
+
+sleep(3)
+print(Fore.YELLOW)
+print('In a one-horse open sleigh. Oh!')
+
+sleep(3)
+print(Fore.CYAN)
+print('Jingle bells, jingle bells,')
+
+sleep(3)
+print(Fore.RED)
+print('Jingle all the way!')
+
+sleep(2)
+print(Fore.GREEN)
+print('O what fun it is to ride')
+
+sleep(3)
+print(Fore.YELLOW)
+print('In a one-horse open sleigh')
+
+sleep(5)
+print(Fore.CYAN)
+print('Всех с новым годом, друзья и подруги!')
+sleep(5)
+print('Всем счастья, здоровья, подарков и удачи!')
+sleep(5)
+print('С новым 2023 годом!')
+sleep(5)
+print('С новыми приключениями!...')
+sleep(4)
